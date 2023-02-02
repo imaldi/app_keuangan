683b7a7e607119d5023377438e47a932af0965c5
imaldi <aldiirsanmajid@gmail.com>
2023-02-03 00:19:39 +0700

    done display Masuk Screen

--- lib/presentation/screens/masuk_screen.dart
+++ lib/presentation/screens/masuk_screen.dart
@@ -38,207 +38,220 @@ class MasukScreenState extends State<MasukScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: const Text("Masuk"),
         elevation: 0,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              width: double.infinity,
-              child: Material(
-                  elevation: 2,
-                  child: Container(
-                    padding: EdgeInsets.symmetric(
-                        vertical: sizeMedium,
-                        horizontal: orientedWidthScreen(context,
-                            portraitRatio: 0.3, landscapeRatio: 0.4)),
-                    height: orientedHeightScreen(context,
-                        portraitRatio: 0.1, landscapeRatio: 0.15),
-                    color: Colors.white,
-                    child: DropdownButtonHideUnderline(
-                      child: DropdownButton2(
-                        isDense: true,
-                        hint: const Text(
-                          'Select Item',
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            color: primaryColor,
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Material(
+                    elevation: 2,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                          vertical: sizeMedium,
+                          horizontal: orientedWidthScreen(context,
+                              portraitRatio: 0.3, landscapeRatio: 0.4)),
+                      height: orientedHeightScreen(context,
+                          portraitRatio: 0.1, landscapeRatio: 0.15),
+                      color: Colors.white,
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton2(
+                          isDense: true,
+                          hint: const Text(
+                            'Select Item',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: primaryColor,
+                            ),
+                            overflow: TextOverflow.ellipsis,
                           ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        items: items
-                            .map((item) => DropdownMenuItem<String>(
-                                  value: item,
-                                  child: Text(
-                                    item,
-                                    style: const TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
+                          items: items
+                              .map((item) => DropdownMenuItem<String>(
+                                    value: item,
+                                    child: Text(
+                                      item,
+                                      style: const TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                      overflow: TextOverflow.ellipsis,
                                     ),
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ))
-                            .toList(),
-                        value: selectedValue,
-                        onChanged: (value) {
-                          setState(() {
-                            selectedValue = value as String;
-                          });
-                        },
-                        icon: SvgPicture.asset(iconDropdownPath),
-                        iconSize: sizeMedium,
-                        iconEnabledColor: primaryColor,
-                        iconDisabledColor: Colors.grey,
-                        buttonPadding: const EdgeInsets.only(
-                            left: sizeMedium, right: sizeMedium),
-                        buttonDecoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(14),
-                          border: Border.all(
-                            color: Colors.black26,
+                                  ))
+                              .toList(),
+                          value: selectedValue,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedValue = value as String;
+                            });
+                          },
+                          icon: SvgPicture.asset(iconDropdownPath),
+                          iconSize: sizeMedium,
+                          iconEnabledColor: primaryColor,
+                          iconDisabledColor: Colors.grey,
+                          buttonPadding: const EdgeInsets.only(
+                              left: sizeMedium, right: sizeMedium),
+                          buttonDecoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(14),
+                            border: Border.all(
+                              color: Colors.black26,
+                            ),
+                            color: accentColor,
                           ),
-                          color: accentColor,
+                          buttonElevation: 2,
+                          itemHeight: 40,
+                          itemPadding: const EdgeInsets.only(left: 14, right: 14),
+                          dropdownMaxHeight: 200,
+                          dropdownWidth: 200,
+                          dropdownPadding: null,
+                          dropdownDecoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(14),
+                            color: accentColor,
+                          ),
+                          dropdownElevation: 8,
+                          scrollbarRadius: const Radius.circular(40),
+                          scrollbarThickness: 6,
+                          scrollbarAlwaysShow: true,
+                          offset: const Offset(-20, 0),
                         ),
-                        buttonElevation: 2,
-                        itemHeight: 40,
-                        itemPadding: const EdgeInsets.only(left: 14, right: 14),
-                        dropdownMaxHeight: 200,
-                        dropdownWidth: 200,
-                        dropdownPadding: null,
-                        dropdownDecoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(14),
-                          color: accentColor,
-                        ),
-                        dropdownElevation: 8,
-                        scrollbarRadius: const Radius.circular(40),
-                        scrollbarThickness: 6,
-                        scrollbarAlwaysShow: true,
-                        offset: const Offset(-20, 0),
                       ),
+                    )),
+              ),
+              const SizedBox(
+                height: sizeMedium,
+              ),
+              const CustomText(
+                "Start Date",
+                color: Colors.white,
+              ),
+              RoundedContainer(
+                  margin: const EdgeInsets.all(sizeNormal),
+                  child: GestureDetector(
+                    onTap: () async {
+                      var newDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100));
+                      setState(() {
+                        tanggalController.text = DateFormat("dd/MM/yyyy")
+                            .format(newDate ?? DateTime.now())
+                            .toString();
+                      });
+                    },
+                    child: NoUnderlineTextFormField(
+                      controller: tanggalController,
+                      enabled: false,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(color: primaryColor),
                     ),
                   )),
-            ),
-            const SizedBox(
-              height: sizeMedium,
-            ),
-            const CustomText(
-              "Start Date",
-              color: Colors.white,
-            ),
-            RoundedContainer(
-                margin: const EdgeInsets.all(sizeNormal),
-                child: GestureDetector(
-                  onTap: () async {
-                    var newDate = await showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(2000),
-                        lastDate: DateTime(2100));
-                    setState(() {
-                      tanggalController.text = DateFormat("dd/MM/yyyy")
-                          .format(newDate ?? DateTime.now())
-                          .toString();
-                    });
-                  },
-                  child: NoUnderlineTextFormField(
-                    controller: tanggalController,
-                    enabled: false,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(color: primaryColor),
-                  ),
-                )),
-            const CustomText(
-              "Input",
-              color: Colors.white,
-            ),
-            RoundedContainer(
-                margin: const EdgeInsets.all(sizeNormal),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: NoUnderlineTextFormField(
-                        style: const TextStyle(color: primaryColor),
-                        decoration: const InputDecoration(
-                          hintStyle: TextStyle(color: primaryColor),
-                          hintText: "0",
-                        ),
-                        keyboardType: TextInputType.number,
-                        textAlign: TextAlign.right,
-                        controller: inputController,
-                      ),
-                    ),
-                    const MainCurrencyDropdown(),
-                  ],
-                )),
-            const CustomText(
-              "Photo",
-              color: Colors.white,
-            ),
-            RoundedContainer(
-                margin: const EdgeInsets.all(sizeNormal),
-                padding: const EdgeInsets.symmetric(horizontal: sizeMedium),
-                child: IntrinsicHeight(
+              const CustomText(
+                "Input",
+                color: Colors.white,
+              ),
+              RoundedContainer(
+                  margin: const EdgeInsets.all(sizeNormal),
                   child: Row(
                     children: [
                       Expanded(
-                          child: RoundedContainer(
-                        constraints: const BoxConstraints(minHeight: 80),
-                        margin: const EdgeInsets.symmetric(
-                            vertical: sizeNormal, horizontal: sizeSmall),
-                        boxDecoration: const BoxDecoration(color: accentColor),
-                        child: Column(
-                          children: const [
-                            Icon(
-                              Icons.camera_alt_outlined,
-                              color: primaryColor,
-                            ),
-                            CustomText(
-                              "Tambahkan Foto",
-                              textAlign: TextAlign.center,
-                              color: primaryColor,
-                            ),
-                          ],
+                        child: NoUnderlineTextFormField(
+                          style: const TextStyle(color: primaryColor),
+                          decoration: const InputDecoration(
+                            hintStyle: TextStyle(color: primaryColor),
+                            hintText: "0",
+                          ),
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.right,
+                          controller: inputController,
                         ),
-                      )),
-                      Expanded(
-                          child: RoundedContainer(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: sizeNormal, horizontal: sizeSmall),
-                        boxDecoration:
-                            BoxDecoration(color: accentColor.withAlpha(99)),
-                        child: Container(),
-                      )),
-                      Expanded(
-                          child: RoundedContainer(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: sizeNormal, horizontal: sizeSmall),
-                        boxDecoration:
-                            BoxDecoration(color: accentColor.withAlpha(99)),
-                        child: Container(),
-                      )),
-                      Expanded(
-                          child: RoundedContainer(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: sizeNormal, horizontal: sizeSmall),
-                        boxDecoration:
-                            BoxDecoration(color: accentColor.withAlpha(99)),
-                        child: Container(),
-                      )),
+                      ),
+                      const MainCurrencyDropdown(),
                     ],
-                  ),
-                )),
-            const CustomText(
-              "Keterangan",
-              color: Colors.white,
+                  )),
+              const CustomText(
+                "Photo",
+                color: Colors.white,
+              ),
+              RoundedContainer(
+                  margin: const EdgeInsets.all(sizeNormal),
+                  padding: const EdgeInsets.symmetric(horizontal: sizeMedium),
+                  child: IntrinsicHeight(
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: RoundedContainer(
+                          constraints: const BoxConstraints(minHeight: 80),
+                          margin: const EdgeInsets.symmetric(
+                              vertical: sizeNormal, horizontal: sizeSmall),
+                          boxDecoration: const BoxDecoration(color: accentColor),
+                          child: Column(
+                            children: const [
+                              Icon(
+                                Icons.camera_alt_outlined,
+                                color: primaryColor,
+                              ),
+                              CustomText(
+                                "Tambahkan Foto",
+                                textAlign: TextAlign.center,
+                                color: primaryColor,
+                              ),
+                            ],
+                          ),
+                        )),
+                        Expanded(
+                            child: RoundedContainer(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: sizeNormal, horizontal: sizeSmall),
+                          boxDecoration:
+                              BoxDecoration(color: accentColor.withAlpha(99)),
+                          child: Container(),
+                        )),
+                        Expanded(
+                            child: RoundedContainer(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: sizeNormal, horizontal: sizeSmall),
+                          boxDecoration:
+                              BoxDecoration(color: accentColor.withAlpha(99)),
+                          child: Container(),
+                        )),
+                        Expanded(
+                            child: RoundedContainer(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: sizeNormal, horizontal: sizeSmall),
+                          boxDecoration:
+                              BoxDecoration(color: accentColor.withAlpha(99)),
+                          child: Container(),
+                        )),
+                      ],
+                    ),
+                  )),
+              const CustomText(
+                "Keterangan",
+                color: Colors.white,
+              ),
+              const RoundedContainer(
+                  margin: EdgeInsets.all(sizeNormal),
+                  child: NoUnderlineTextFormField()),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: sizeMedium),
+              child: ElevatedButton(
+                child: Text("Submit"),
+                onPressed: () {},
+              ),
             ),
-            const RoundedContainer(
-                margin: EdgeInsets.all(sizeNormal),
-                child: NoUnderlineTextFormField()),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
